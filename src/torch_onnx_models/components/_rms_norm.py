f10bb1a70fe5a74a80915bf7781a32b2f915a07b
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-08 16:41:45 -0700

    Create unit tests
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/components/_rms_norm.py
+++ src/torch_onnx_models/components/_rms_norm.py
@@ -2,19 +2,18 @@ from __future__ import annotations
 
 import torch
 from torch import nn
-from torch_onnx_models import _config
 
 
 class RMSNorm(nn.Module):
-    def __init__(self, *, hidden_size: int, eps: float = 1e-6, config: _config.Config):
+    def __init__(self, *, hidden_size: int, eps: float = 1e-6, mode: str):
         super().__init__()
-        self.weight = nn.Parameter(torch.tensor(1.0))
+        self.weight = nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = eps
-        self._config = config
+        self._config = mode
 
     def forward(self, hidden_states):
         if torch.onnx.is_in_onnx_export():
-            if self._config.opset == "ort":
+            if self._config == "ort":
                 return torch.onnx.ops.symbolic(
                     "com.microsoft::SkipSimplifiedLayerNormalization",
                     [hidden_states, None, self.weight],
@@ -23,16 +22,8 @@ class RMSNorm(nn.Module):
                     shape=hidden_states.shape,
                     version=1,
                 )
-            else:
-                # This will produce the correct ONNX standard ops based on the opset requested
-                return torch.ops.aten.rms_norm(
-                    hidden_states,
-                    (hidden_states.size(-1),),
-                    self.weight,
-                    self.variance_epsilon,
-                )
-        else:
-            # rms_norm(Tensor input, SymInt[] normalized_shape, Tensor? weight=None, float? eps=None) -> Tensor
-            return torch.ops.aten.rms_norm(
-                hidden_states, (hidden_states.size(-1),), self.weight, self.variance_epsilon
-            )
+        # This will produce the correct ONNX standard ops based on the opset requested
+        # rms_norm(Tensor input, SymInt[] normalized_shape, Tensor? weight=None, float? eps=None) -> Tensor
+        return torch.ops.aten.rms_norm(
+            hidden_states, (hidden_states.size(-1),), self.weight, self.variance_epsilon
+        )
---
+++ test/rms_norm_test.py
@@ -0,0 +1,36 @@
+import unittest
+
+import parameterized
+import torch
+from torch.onnx._internal.exporter import _testing as onnx_testing
+from torch_onnx_models.components._rms_norm import RMSNorm
+
+
+class TestRMSNorm(unittest.TestCase):
+    @parameterized.parameterized.expand(
+        [
+            ("ort", 18),
+            ("onnx", 18),
+            ("onnx", 23),
+        ]
+    )
+    def test_export(self, mode: str, opset_version: int):
+        hidden_size = 768
+        model = RMSNorm(hidden_size=hidden_size, mode=mode)
+        model.eval()
+
+        input = torch.randn(2, 128, 768)
+
+        onnx_program = torch.onnx.export(
+            model,
+            (input,),
+            opset_version=opset_version,
+            dynamic_shapes={"hidden_states": {0: "batch_size", 1: "seq_len"}},
+            dynamo=True,
+            verbose=False,
+        )
+        onnx_testing.assert_onnx_program(onnx_program)
+
+
+if __name__ == "__main__":
+    unittest.main()
