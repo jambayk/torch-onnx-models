eabc10061d912ddf906d57c242f25c64778cdef0
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-18 19:09:47 -0700

    Create tests for replace_subgraph and the remove barrier pass
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/onnx_passes/__init__.py
+++ src/torch_onnx_models/onnx_passes/__init__.py
@@ -1,3 +1,11 @@
-__all__ = ["AssignNamesPass"]
+__all__ = [
+    "AssignNamesPass",
+    "RemoveBarrierPass",
+    "CollectOpsetsPass",
+    "replace_subgraph",
+]
 
 from ._assign_names import AssignNamesPass
+from ._barrier_removal import RemoveBarrierPass
+from ._collect_opsets import CollectOpsetsPass
+from ._subgraph_replacement import replace_subgraph
--- test/onnx_passes_test.py
+++ test/onnx_passes_test.py
@@ -3,7 +3,12 @@ from __future__ import annotations
 import unittest
 
 import torch
+
 from torch_onnx_models import onnx_passes
+from torch_onnx_models.components._activations import (
+    MsftQuickGELUActivation,
+    QuickGELUActivation,
+)
 from torch_onnx_models.components._rms_norm import RMSNorm
 
 
@@ -12,7 +17,7 @@ class AssignNamesPassTest(unittest.TestCase):
         class Model(torch.nn.Module):
             def __init__(self, hidden_size: int):
                 super().__init__()
-                self.rms_norm = RMSNorm(hidden_size=hidden_size, mode="onnx")
+                self.rms_norm = RMSNorm(hidden_size=hidden_size)
 
             def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
                 return self.rms_norm(hidden_states)
@@ -35,6 +40,44 @@ class AssignNamesPassTest(unittest.TestCase):
         pass_result = onnx_passes.AssignNamesPass()(onnx_program.model)
         print(pass_result.model)
         # TODO: Actually assert some things
+
+
+class RemoveBarrierPassTest(unittest.TestCase):
+    def test_pass(self):
+        x = torch.randn(2, 3)
+        onnx_program = torch.onnx.export(
+            QuickGELUActivation(), (x,), dynamo=True, verbose=False
+        )
+        model = onnx_program.model
+        self.assertIn("Barrier", [node.op_type for node in model.graph])
+        onnx_passes.RemoveBarrierPass()(model)
+        self.assertEqual(len(model.graph), 3)
+        self.assertNotIn("Barrier", [node.op_type for node in model.graph])
+
+
+class SubgraphReplacementTest(unittest.TestCase):
+    def test_pass(self):
+        x = torch.randn(2, 3)
+        model = torch.onnx.export(
+            QuickGELUActivation(), (x,), dynamo=True, verbose=False
+        ).model
+        ort_model = torch.onnx.export(
+            MsftQuickGELUActivation(), (x,), dynamo=True, verbose=False
+        ).model
+        region_start = next(node for node in model.graph if node.op_type == "Barrier")
+        region_end = next(
+            node for node in reversed(model.graph) if node.op_type == "Barrier"
+        )
+        self.assertNotIn("QuickGelu", [node.op_type for node in model.graph])
+        onnx_passes.replace_subgraph(
+            model, region_start.inputs, region_end.outputs, ort_model.graph
+        )
+        self.assertIn("QuickGelu", [node.op_type for node in model.graph])
+        print(model)
+
+        onnx_passes.RemoveBarrierPass()(model)
+        self.assertEqual(len(model.graph), 1)
+        print(model)
 
 
 if __name__ == "__main__":
