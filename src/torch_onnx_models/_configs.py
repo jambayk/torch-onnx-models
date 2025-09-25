f1596bbfbb01d93d6ea48074fea7aaf901470ba6
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-25 09:49:40 -0700

    config
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/_configs.py
+++ src/torch_onnx_models/_configs.py
@@ -8,23 +8,23 @@ import torch
 @dataclasses.dataclass
 class ArchitectureConfig:
     # Config from transformers
-    head_dim: int | None = None
-    num_attention_heads: int | None = None
-    num_key_value_heads: int | None = None
-    num_hidden_layers: int | None = None
-    vocab_size: int | None = None
-    hidden_size: int | None = None
-    intermediate_size: int | None = None
+    head_dim: int = -42
+    num_attention_heads: int = -42
+    num_key_value_heads: int = -42
+    num_hidden_layers: int = -42
+    vocab_size: int = -42
+    hidden_size: int = -42
+    intermediate_size: int = -42
     hidden_act: str | None = None
-    pad_token_id: int | None = None
-    rms_norm_eps: float | None = None
+    pad_token_id: int = -42
+    rms_norm_eps: float = 1e-6
     attention_bias: bool = False
     mlp_bias: bool = False
 
     # Rotary embedding config
-    rope_type: str | None = None
+    rope_type: str = "default"
     rope_theta: float = 10_000.0
-    max_position_embeddings: int | None = None
+    max_position_embeddings: int = -42
 
 
 @dataclasses.dataclass
