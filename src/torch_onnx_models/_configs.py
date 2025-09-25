4fd2f9eaf0a7963f8d2abe4f5d99695b59db20aa
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-25 09:46:56 -0700

    default config
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/_configs.py
+++ src/torch_onnx_models/_configs.py
@@ -8,32 +8,23 @@ import torch
 @dataclasses.dataclass
 class ArchitectureConfig:
     # Config from transformers
-    head_dim: int
-    num_attention_heads: int
-    num_key_value_heads: int
-    num_hidden_layers: int
-    vocab_size: int
-    hidden_size: int
-    intermediate_size: int
-    hidden_act: str
-    pad_token_id: int
-    rms_norm_eps: float
-    attention_bias: bool  # = False
-    mlp_bias: bool  # = False
+    head_dim: int | None = None
+    num_attention_heads: int | None = None
+    num_key_value_heads: int | None = None
+    num_hidden_layers: int | None = None
+    vocab_size: int | None = None
+    hidden_size: int | None = None
+    intermediate_size: int | None = None
+    hidden_act: str | None = None
+    pad_token_id: int | None = None
+    rms_norm_eps: float | None = None
+    attention_bias: bool = False
+    mlp_bias: bool = False
 
     # Rotary embedding config
-    rope_type: str | None  # = None
-    max_position_embeddings: int
-
-    @classmethod
-    def from_default(cls, **kwargs) -> ArchitectureConfig:
-        defaults = {
-            "attention_bias": False,
-            "mlp_bias": False,
-            "rope_type": None,
-        }
-        defaults.update(kwargs)
-        return cls(**defaults)
+    rope_type: str | None = None
+    rope_theta: float = 10_000.0
+    max_position_embeddings: int | None = None
 
 
 @dataclasses.dataclass
