98d742c9ba8437dcaa525480d67f7ecb9381911d
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-25 09:47:01 -0700

    create_attention_bias
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/components/__init__.py
+++ src/torch_onnx_models/components/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
     "apply_rope",
     "Attention",
+    "create_attention_bias",
     "get_activation",
     "LlamaMLP",
     "Phi3MLP",
@@ -8,6 +9,7 @@ __all__ = [
 ]
 
 from torch_onnx_models.components._attention import Attention
+from torch_onnx_models.components._attention_utils import create_attention_bias
 from torch_onnx_models.components._activations import get_activation
 from torch_onnx_models.components._rms_norm import RMSNorm
 from torch_onnx_models.components._rotary_embedding_utils import apply_rope
