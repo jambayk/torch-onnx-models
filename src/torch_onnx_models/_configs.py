8c8be8f85ad3b01cfa817006342b81da92aae098
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-29 18:28:42 -0700

    update config
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/_configs.py
+++ src/torch_onnx_models/_configs.py
@@ -32,56 +32,32 @@ SUPPORTED_ARCHITECTURES = {
 DEFAULT_INT = -42
 
 
-# self.vocab_size = vocab_size
-# self.max_position_embeddings = max_position_embeddings
-# self.hidden_size = hidden_size
-# self.intermediate_size = intermediate_size
-# self.num_hidden_layers = num_hidden_layers
-# self.num_attention_heads = num_attention_heads
-
-# # for backward compatibility
-# if num_key_value_heads is None:
-#     num_key_value_heads = num_attention_heads
-
-# self.num_key_value_heads = num_key_value_heads
-# self.hidden_act = hidden_act
-# self.initializer_range = initializer_range
-# self.rms_norm_eps = rms_norm_eps
-# self.pretraining_tp = pretraining_tp
-# self.use_cache = use_cache
-# self.rope_theta = rope_theta
-# self.rope_scaling = rope_scaling
-# self.attention_bias = attention_bias
-# self.attention_dropout = attention_dropout
-# self.mlp_bias = mlp_bias
-# self.head_dim = head_dim if head_dim is not None else self.hidden_size // self.num_attention_heads
-# # Validate the correctness of rotary position embeddings parameters
-# # BC: if there is a 'type' field, copy it it to 'rope_type'.
-# if self.rope_scaling is not None and "type" in self.rope_scaling:
-#     self.rope_scaling["rope_type"] = self.rope_scaling["type"]
-# rope_config_validation(self)
-
 @dataclasses.dataclass
 class ArchitectureConfig:
     # Config from transformers
-    head_dim: int = DEFAULT_INT
-    num_attention_heads: int = DEFAULT_INT
-    num_key_value_heads: int = DEFAULT_INT
-    num_hidden_layers: int = DEFAULT_INT
     vocab_size: int = DEFAULT_INT
+    max_position_embeddings: int = DEFAULT_INT
     hidden_size: int = DEFAULT_INT
     intermediate_size: int = DEFAULT_INT
+    num_hidden_layers: int = DEFAULT_INT
+    num_attention_heads: int = DEFAULT_INT
+    num_key_value_heads: int = DEFAULT_INT
     hidden_act: str | None = None
-    pad_token_id: int = DEFAULT_INT
+
     rms_norm_eps: float = 1e-6
-    attention_bias: bool = False
-    mlp_bias: bool = False
 
     # Rotary embedding config
     rope_type: str = "default"
     rope_theta: float = 10_000.0
-    max_position_embeddings: int = DEFAULT_INT
+    rope_scaling: dict | None = None  # Not supported yet
     partial_rotary_factor: float = 1.0  # 1.0 means no partial RoPE
+
+    attention_bias: bool = False
+    mlp_bias: bool = False
+
+    head_dim: int = DEFAULT_INT
+
+    pad_token_id: int = DEFAULT_INT
 
     @classmethod
     def from_transformers(cls, config) -> ArchitectureConfig:
