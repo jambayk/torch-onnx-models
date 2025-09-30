7fb99386f1e5ae5ef94842a7556062141ac8cf11
Justin Chu <justinchuby@users.noreply.github.com>
2025-09-29 17:33:02 -0700

    config
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/_configs.py
+++ src/torch_onnx_models/_configs.py
@@ -29,6 +29,35 @@ SUPPORTED_ARCHITECTURES = {
 }
 
 
+# self.vocab_size = vocab_size
+# self.max_position_embeddings = max_position_embeddings
+# self.hidden_size = hidden_size
+# self.intermediate_size = intermediate_size
+# self.num_hidden_layers = num_hidden_layers
+# self.num_attention_heads = num_attention_heads
+
+# # for backward compatibility
+# if num_key_value_heads is None:
+#     num_key_value_heads = num_attention_heads
+
+# self.num_key_value_heads = num_key_value_heads
+# self.hidden_act = hidden_act
+# self.initializer_range = initializer_range
+# self.rms_norm_eps = rms_norm_eps
+# self.pretraining_tp = pretraining_tp
+# self.use_cache = use_cache
+# self.rope_theta = rope_theta
+# self.rope_scaling = rope_scaling
+# self.attention_bias = attention_bias
+# self.attention_dropout = attention_dropout
+# self.mlp_bias = mlp_bias
+# self.head_dim = head_dim if head_dim is not None else self.hidden_size // self.num_attention_heads
+# # Validate the correctness of rotary position embeddings parameters
+# # BC: if there is a 'type' field, copy it it to 'rope_type'.
+# if self.rope_scaling is not None and "type" in self.rope_scaling:
+#     self.rope_scaling["rope_type"] = self.rope_scaling["type"]
+# rope_config_validation(self)
+
 @dataclasses.dataclass
 class ArchitectureConfig:
     # Config from transformers
@@ -53,8 +82,10 @@ class ArchitectureConfig:
 
     @classmethod
     def from_transformers(cls, config) -> ArchitectureConfig:
-        if getattr(config, "model_type") != "llama":
-            raise ValueError("Only llama model is supported yet")
+        if config.model_type not in SUPPORTED_ARCHITECTURES:
+            raise ValueError(
+                f"Model type '{config.model_type}' not supported. Supported architectures: {SUPPORTED_ARCHITECTURES}"
+            )
 
         options = dict(
             head_dim=config.hidden_size // config.num_attention_heads,
