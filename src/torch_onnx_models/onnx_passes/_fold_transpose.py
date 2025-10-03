2777dc7490a1d5ba8a33d3ec8916480107f72296
Justin Chu <justinchuby@users.noreply.github.com>
2025-10-02 20:59:02 -0700

    Implement
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/onnx_passes/_fold_transpose.py
+++ src/torch_onnx_models/onnx_passes/_fold_transpose.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import onnx_ir as ir
 import torch
-
+from onnx_ir import tensor_adapters
 
 
 class FoldTransposePass(ir.passes.InPlacePass):
@@ -22,21 +22,33 @@ class FoldTransposePass(ir.passes.InPlacePass):
             assert len(node.inputs) == 1
             assert len(node.outputs) == 1
 
-            perm_attr = node.attributes.get_ints("perm")
-
             assert value.const_value is not None
+            initializer = value.const_value
+            shape = initializer.shape
+
+            perm = node.attributes.get_ints("perm", reversed(range(len(shape))))
 
             # Create a lazy transposed tensor
-            raw_tensor = value.const_value.raw
-            if isinstance(raw_tensor, torch.Tensor):
-                def tensor_func(tensor=raw_tensor):
-                    tensor = tensor.transpose(*perm_attr)
-                    return tensor_adapters.TorchTensor(tensor, name=name)
+            torch_tensor = initializer.raw
+            if isinstance(torch_tensor, torch.Tensor):
 
-                ir_tensor = ir.LazyTensor(
-                    tensor_func,
-                    dtype=onnx_dtype,
-                    shape=ir.Shape(tensor.shape),
-                    name=name,
-                )
-            transposed_tensor = value.const_value
+                def tensor_func(tensor=torch_tensor):
+                    tensor = tensor.permute(*perm)
+                    return tensor_adapters.TorchTensor(tensor, name=name)
+            else:
+
+                def tensor_func(tensor=initializer):
+                    array = tensor.numpy()
+                    return tensor_adapters.TorchTensor(
+                        torch.from_numpy(array).permute(*perm), name=name
+                    )
+
+            value.const_value = ir.LazyTensor(
+                tensor_func,
+                dtype=initializer.dtype,
+                shape=ir.Shape(shape),
+                name=name,
+            )
+
+            modified = True
+        return ir.passes.PassResult(model, modified=modified)
