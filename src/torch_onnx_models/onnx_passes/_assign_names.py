20660ab21a5ce064ac1d79bed5523599aeb67985
Justin Chu <justinchuby@users.noreply.github.com>
2025-10-06 11:12:20 -0700

    Rename nodes to add scope as prefix (#47)
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/torch_onnx_models/onnx_passes/_assign_names.py
+++ src/torch_onnx_models/onnx_passes/_assign_names.py
@@ -12,6 +12,19 @@ from onnx_ir.passes._pass_infra import PassResult
 logger = logging.getLogger(__name__)
 
 
+def _get_scoped_prefix(name_scopes: list[str]) -> str:
+    # Remove common prefixes between consecutive scopes
+    processed_scopes = []
+    for i, scope in enumerate(name_scopes):
+        if i == 0:
+            processed_scopes.append(scope)
+        else:
+            prev_scope = name_scopes[i - 1]
+            processed_scopes.append(scope.removeprefix(prev_scope).lstrip("."))
+
+    return "/".join(processed_scopes)
+
+
 class AssignNamesPass(ir.passes.InPlacePass):
     def call(self, model: ir.Model) -> PassResult:
         modified = False
@@ -22,26 +35,23 @@ class AssignNamesPass(ir.passes.InPlacePass):
                     ast.literal_eval(node.metadata_props["pkg.torch.onnx.name_scopes"]),
                 )
                 name_scopes.pop()  # Remove self name
+                prefix = _get_scoped_prefix(name_scopes)
+
+                # Rename node
+                if prefix:
+                    node.name = f"{prefix}/{node.name}"
+                    modified = True
+
+                # Rename outputs
                 for output in node.outputs:
                     if (
                         not output.is_graph_output()
                         and output.name is not None
                         and output.name != ""
                     ):
-                        # Remove common prefixes between consecutive scopes
-                        processed_scopes = []
-                        for i, scope in enumerate(name_scopes):
-                            if i == 0:
-                                processed_scopes.append(scope)
-                            else:
-                                prev_scope = name_scopes[i - 1]
-                                processed_scopes.append(
-                                    scope.removeprefix(prev_scope).lstrip(".")
-                                )
-                        processed_scopes.append(output.name)
-
-                        scoped_name = "/".join(processed_scopes)
-                        logger.debug("Renaming %r to %r", output.name, scoped_name)
-                        output.name = scoped_name
-                        modified = True
+                        if prefix:
+                            scoped_name = f"{prefix}/{output.name}"
+                            logger.debug("Renaming %r to %r", output.name, scoped_name)
+                            output.name = scoped_name
+                            modified = True
         return PassResult(model, modified)
