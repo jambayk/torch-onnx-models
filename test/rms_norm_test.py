38d8c18bf882c399f279d26848f6bd30e7b269a0
Justin Chu <justinchuby@users.noreply.github.com>
2025-10-08 20:05:34 -0700

    Fix RMSNorm test
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- test/rms_norm_test.py
+++ test/rms_norm_test.py
@@ -3,6 +3,8 @@ import unittest
 import parameterized
 import torch
 from torch.onnx._internal.exporter import _testing as onnx_testing
+
+from torch_onnx_models import _configs
 from torch_onnx_models.components._rms_norm import RMSNorm
 
 
@@ -15,12 +17,14 @@ class RMSNormTest(unittest.TestCase):
     )
     def test_export(self, opset_version: int):
         hidden_size = 768
-        model = RMSNorm(hidden_size=hidden_size)
+        model = RMSNorm(hidden_size=hidden_size, config=_configs.ArchitectureConfig())
         model.eval()
 
         input = torch.randn(2, 128, 768)
 
-        ref_model = RMSNorm(hidden_size=hidden_size)
+        ref_model = RMSNorm(
+            hidden_size=hidden_size, config=_configs.ArchitectureConfig()
+        )
         ref_model.eval()
 
         ref_ep = torch.export.export(ref_model, (input,))
